718e04b4ac787fe2f2a03be19de9e22882b31fa6
crazy2be <code@jmcgirr.com>
2015-09-19 19:09:24 -0700

    Initial 'working' version.

---
+++ main.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+
+var request = require("request");
+// http://stackoverflow.com/questions/9920208/expressjs-raw-body
+app.use (function(req, res, next) {
+    var data='';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+       data += chunk;
+    });
+
+    req.on('end', function() {
+        req.body = data;
+        next();
+    });
+});
+app.all("/*", function(req, res) {
+ //
+ //console.log("Got request", req);
+ var url = unescape(req.path).slice(1);
+ console.log("Requesting url", url)
+
+ var opts = {
+  url: 'https://go-ai.firebaseio.com/' + url,
+  method: req.method,
+  body: req.body,
+ };
+ console.log(opts);
+ request(opts, function(err, response, body) {
+	 //console.log(response);
+  res.send(response.body);
+ })
+});
+
+app.listen(2500, function() {
+ console.log("Started server");
+});
